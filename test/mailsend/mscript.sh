22b710a68c9363c96dda532b1744e2bb97439879
Steve Mansour <sman@stevemansour.com>
2017-04-20 14:41:10 -0700

    FIX: change 'mailtester' to 'mailsend'

--- test/mailsend/mscript.sh
+++ test/mailsend/mscript.sh
@@ -6,19 +6,19 @@
 #./mailsend -h "http://localhost:8275/" -q MojoTest -subject "Big Mojo Test" -a perks.pdf
 #./mailsend -h "http://localhost:8275/" -q MojoTest -subject "Perks Mojo Test" -a perks.pdf -b msg.html
 
-./mailtester -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q MojoTest -subject "Big Mojo Test" -a perks.pdf
+./mailsend -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q MojoTest -subject "Big Mojo Test" -a perks.pdf
 
-#./mailtester -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q MojoTest -subject "Perks - Mojo Email Test" -a perks.pdf -b msg.html
+#./mailsend -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q MojoTest -subject "Perks - Mojo Email Test" -a perks.pdf -b msg.html
 
 
 
 #--------------------------------------------
 #  AMAZON:  Steve  +  Amazon Test Accounts
 #--------------------------------------------
-#./mailtester -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q AmazonTest -subject "Big Mojo Test 3" -a perks.pdf
+#./mailsend -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q AmazonTest -subject "Big Mojo Test 3" -a perks.pdf
 
 
 #------------------------------------------------------
 #  ACCORD:  Steve, Joe, Melissa,  Amazon Test Accounts
 #------------------------------------------------------
-#./mailtester -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q AccordTest -subject "Big Mojo Test to Accord" -a perks.pdf
+#./mailsend -h "http://ec2-54-152-108-202.compute-1.amazonaws.com:8275/" -q AccordTest -subject "Big Mojo Test to Accord" -a perks.pdf
